efe267bf19876137ba4d5e7ef019befb08390d1c
Maxime Haineault <haineault@gmail.com>
2011-11-19 00:39:12 -0500

    Updated file timestamps

---
+++ checkin/templatetags/checkin_tags.py
@@ -0,0 +1,34 @@
+import datetime
+
+from django.core.urlresolvers import reverse
+from django import template
+
+from checkin.models import CheckinPlace, Checkin
+
+register = template.Library()
+
+
+
+class CheckedInWithinLastNode(template.Node):
+    def __init__(self, hours, place, user, varname):
+        self.hours = float(hours)
+        self.place = template.Variable(place)
+        self.user = template.Variable(user)
+        self.varname = varname
+    
+    def render(self, context):
+        place_id = self.place.resolve(context).id
+        qs = Checkin.objects.filter(place_id=place_id, user_id=self.user.resolve(context).id,
+                date__gte=(datetime.datetime.now() - datetime.timedelta(hours=self.hours)))
+        context[self.varname] = qs.count() and qs[0] or False
+        return ''
+
+
+@register.tag()
+def checked_in_within_last(parser, token):
+    bits = token.contents.split()
+    if len(bits) != 6:
+        raise template.TemplateSyntaxError, "checked_in_within_last tag takes exactly three arguments"
+    if bits[4] != 'as':
+        raise template.TemplateSyntaxError, "second argument to the checked_in_within_last tag must be 'as'"
+    return CheckedInWithinLastNode(bits[1], bits[2], bits[3], bits[5])
