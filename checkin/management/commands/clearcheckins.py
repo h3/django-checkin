f811e6041c3781f53f3bb6e1e93cbb84959a1837
Maxime Haineault <haineault@gmail.com>
2012-01-16 13:40:16 -0500

    ..

---
+++ checkin/management/commands/clearcheckins.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from checkin.models import Checkin
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Clear all checkins (non reversible !)'
+
+    def handle(self, *args, **options):
+        Chekin.objects.all().delete()
+        self.stdout.write('Successfully removed all checkins.')
+
+
