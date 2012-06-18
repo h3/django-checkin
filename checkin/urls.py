44d35974eb3936b53b4e569030f9a45e0ac9a7a5
Krozark <maxime.barbier1991@gmail.com>
2012-06-18 14:31:29 -0400

    ...

--- checkin/urls.py
+++ checkin/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     url(r'^checkin$',  csrf_exempt(CheckinSubmitView.as_view()), name='checkin-submit'),
 )
 
-if settings.DEBUG:
-    urlpatterns = urlpatterns + patterns('',
+#if settings.DEBUG:
+urlpatterns = urlpatterns + patterns('',
     url(r'^map$',      CheckinMapView.as_view(),       name='checkin-map'),
     url(r'^console$',  CheckinConsoleView.as_view(),   name='checkin-console'),
 )
